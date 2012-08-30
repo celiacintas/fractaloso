4217d6c825e20ed595a02b14715dcb01b16d319c
Celita Cintas <celita@localhost.localdomain>
2012-08-30 13:26:23 -0300

    plot listo ... falta scan

--- pygalaxies.py
+++ pygalaxies.py
@@ -8,7 +8,7 @@
 import numpy as np
 import scipy.stats.distributions as dis
 from mayavi import mlab
-from itertools import repeat, izip
+from itertools import repeat, izip, ifilter
 #import pylab as pl
 
 
@@ -31,19 +31,38 @@ class Galaxy:
 
         stars = []
         stars.append(Star(0, neighborhood[0], np.random.randint(1, 15)))
-        for r in radio:
-            map(lambda a: stars.append(Star(a[1], a[0] / float(a[1]),
-                np.random.randint(1, 15))), izip(neighborhood,
-                repeat(r, len(neighborhood))))
-        self.stars = stars
+
+        try:
+            for r in np.arange(1, radio + 1):
+                map(lambda a: stars.append(Star(a[1], a[0] / float(a[1]),
+                    np.random.randint(1, 15))), izip(neighborhood,
+                    repeat(r, len(neighborhood))))
+            self.stars = stars
+        except e:
+            print e
 
     def scanning(self):
         """See state of stars and change formations"""
-        pass
+        #for s in self.stars:
+        neighborhood = ifilter(lambda star: ((s.r + 1) == star.r)
+                               & (star.state == 0), self.stars)
+        #print self.stars
+
+    def plotting(self):
+        """For plotting the stars"""
+        mlab.clf()
+        map(lambda star: mlab.points3d(star.r * np.cos(star.angle),
+            star.r * np.sin(star.angle), star.r * np.cos(star.angle),
+            colormap="copper", scale_factor=.25), self.stars)
+
+        print self.stars
 
 if __name__ == "__main__":
     neighborhood = [0, 60, 120, 180, 240, 330]
-    vals = [np.arange(7), (0.1, 0.2, 0.3, 0.1, 0.1, 0.1, 0.1)]
+    vals = [(0, 1), (0.84, 0.16)]
     distributionFunc = dis.rv_discrete(name='custm', values=vals)
     myGalaxy = Galaxy(1.5, neighborhood)
+    myGalaxy.scanning()
+    myGalaxy.plotting()
+
     mlab.show()
